ee194a2437ce8bbbfb5ea9cfc88d9b26af681ae1
lxd <xdli@thoughtworks.com>
2014-09-30 15:42:03 +0800

    Append `yum update`

--- centos6/install-epel-ius.sh
+++ centos6/install-epel-ius.sh
@@ -3,3 +3,5 @@
 sudo rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 sudo rpm -Uvh http://dl.iuscommunity.org/pub/ius/stable/CentOS/6/x86_64/ius-release-1.0-13.ius.centos6.noarch.rpm
 
+sudo yum update
+
