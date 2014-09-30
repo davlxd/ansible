cc8383664b6dd9204a5caf8be47d1edfa43cbae2
lxd <xdli@thoughtworks.com>
2014-09-30 15:32:46 +0800

    Add EPEL & IUS repo script

---
+++ centos6/install-epel-ius.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+sudo rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+sudo rpm -Uvh http://dl.iuscommunity.org/pub/ius/stable/CentOS/6/x86_64/ius-release-1.0-13.ius.centos6.noarch.rpm
+
